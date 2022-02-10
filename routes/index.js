3b246e1b747d2e91d628027762e731ab5ecd3f10
Laaziri-Abdo <abdolaaziri@gmail.com>
2022-02-10 12:11:52 +0100

    new project

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const usersRouters = require("./users");
-const hotelsRouters = require("./hotels");
-
-const routes = {};
-
-routes.usersRouter = usersRouters;
-routes.hotelsRouter = hotelsRouters;
-
-module.exports = routes;
